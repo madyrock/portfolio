b913401c8cfa5d37204331f980a3edfa7a71daf6
Mudassir Ghazi <amaan.khan848@gmail.com>
2025-04-08 19:42:30 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { LocomotiveScrollProvider } from "react-locomotive-scroll";
-import { useRef } from "react";
-import "locomotive-scroll/dist/locomotive-scroll.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Technology from "./components/Technology";
@@ -10,36 +7,25 @@ import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 
 function App() {
-  const containerRef = useRef(null);
-
   return (
-    <LocomotiveScrollProvider
-      options={{
-        smooth: true,
-        // Add other locomotive-scroll options here
-      }}
-      watch={[/* Add dependencies to reinitialize scroll if needed */]}
-      containerRef={containerRef}
-    >
-      <div data-scroll-container ref={containerRef}>
-        <Navbar />
-        <div className="" id="home" data-scroll-section>
-          <Home />
-        </div>
-        <div id="technology" data-scroll-section>
-          <Technology />
-        </div>
-        <div id="experience" data-scroll-section>
-          <Experience />
-        </div>
-        <div id="projects" data-scroll-section>
-          <Projects />
-        </div>
-        <div id="contact" data-scroll-section>
-          <Contact />
-        </div>
+    <div>
+      <Navbar />
+      <div id="home">
+        <Home />
       </div>
-    </LocomotiveScrollProvider>
+      <div id="technology">
+        <Technology />
+      </div>
+      <div id="experience">
+        <Experience />
+      </div>
+      <div id="projects">
+        <Projects />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
+    </div>
   );
 }
 
